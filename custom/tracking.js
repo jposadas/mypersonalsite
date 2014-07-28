2155167ae1944890ec8687f1e056596d14e609d5
Juan Posadas <jposadas@stanford.edu>
2014-07-28 22:14:25 +0200

    finished tracking and layout

--- custom/tracking.js
+++ custom/tracking.js
@@ -1,15 +1,45 @@
-(function($) {
+(function ($) {
 
-    $(document).ready(function() {
+	var tracking = new Firebase('https://sweltering-fire-7687.firebaseio.com/tracking');
 
-        $.ajax({
-            type: "GET",
-            url: "custom/tracklist.json",
-            dataType: "json"
-        }).done(function(data) {
-            console.log(data);
-        });
+	$(document).ready(function () {
 
-    });
+		$.ajax( { 
+		  url: 'http://freegeoip.net/json/', 
+		  type: 'POST', 
+		  dataType: 'jsonp',
+		  success: function(location) {
+		    tracking.push({ 
+		    	'timestamp': $.now(), 
+		    	'date': getCurrentDate(),
+		    	'country_code': location.country_code,
+		    	'country': location.country_name,
+		    	'ip_address': location.ip
+		    });
+		  }
+		});
+
+		
+
+	});
+
+	var getCurrentDate = function() {
+		var d = new Date();
+
+		var month = d.getMonth()+1;
+		var day = d.getDate();
+		var hour = d.getHours();
+		var minute = d.getMinutes();
+		var second = d.getSeconds();
+
+		var output = d.getFullYear() + '-' +
+		    ((''+month).length<2 ? '0' : '') + month + '-' +
+		    ((''+day).length<2 ? '0' : '') + day + ' ' +
+		    ((''+hour).length<2 ? '0' :'') + hour + ':' +
+		    ((''+minute).length<2 ? '0' :'') + minute + ':' +
+		    ((''+second).length<2 ? '0' :'') + second;
+
+		return output;
+	};
 
 })(jQuery);
