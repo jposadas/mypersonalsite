a5a90f0049490c6e18ee7278f83393445373111b
Juan Posadas <jposadas@stanford.edu>
2014-08-04 18:21:56 +0200

    worked on angular backend and layout

--- custom/controllers.js
+++ custom/controllers.js
@@ -1,10 +1,33 @@
 
 var audioPlayerApp = angular.module('audioPlayerApp', []);
 
-audioPlayerApp.controller('TrackListCtrl', function($scope, $http) {
+audioPlayerApp.controller('TrackListCtrl', ['$scope', '$http', 'AppModel', function($scope, $http, AppModel) {
 
     $http.get('custom/tracklist.json').success(function(data) {
         $scope.tracks = data.tracks;
     });
 
-});
+    $scope.playTrack = function(track) {
+        console.log(track);
+        AppModel.setSelectedTrack(track);
+        AppModel.setIsTrackSelected(true);
+    };
+
+}]);
+
+audioPlayerApp.controller('PlayTrackCtrl', ['$scope', 'AppModel', function($scope, AppModel) {
+    $scope.model = AppModel;
+}]);
+
+audioPlayerApp.service('AppModel', ['$http', function($http) {
+
+    this.selectedTrack = null;
+    this.isTrackSelected = false;
+    this.setSelectedTrack = function(track) {
+        this.selectedTrack = track;
+    };
+    this.setIsTrackSelected = function(isSelected) {
+        this.isTrackSelected = isSelected;
+    }
+
+}]);
