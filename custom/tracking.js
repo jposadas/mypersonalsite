f5062694b719c59b9349ffe752e442719c677102
Juan Posadas <jposadas@stanford.edu>
2014-08-31 11:09:50 +0200

    release

--- custom/tracking.js
+++ custom/tracking.js
@@ -1,6 +1,6 @@
 (function ($) {
 
-	var tracking = new Firebase('https://sweltering-fire-7687.firebaseio.com/tracking_test');
+	var tracking = new Firebase('https://sweltering-fire-7687.firebaseio.com/tracking');
 
 	$(document).ready(function () {
 
